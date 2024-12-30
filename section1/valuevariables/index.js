70f4d0d5fefa43fd9ecac72ff4c401be31659dd8
MLankaoui <marouanelankaoui66@gmail.com>
2024-12-30 11:44:41 +0100

    Add index.html and index.js for JavaScript Fundamentals – Part 1

---
+++ section1/valuevariables/index.js
@@ -0,0 +1,6 @@
+console.log("Jhon Doe");
+console.log(23);
+
+// storing values in variables
+let firstName = "Jhon"; // declaring a variable by storing a string value in the varaiable.
+console.log(firstName); // output: Jhon
